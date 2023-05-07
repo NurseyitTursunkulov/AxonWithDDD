ea8ac2154515314f076ff593745928b99063846e
nurseiit.tursunkulov <nurseiit.tursunkulov@check24.de>
2023-05-07 04:41:25 +0200

    repaired followUserByUsername function

--- src/main/kotlin/com/example/testfordatabase/application/service/ProfilesController.kt
+++ src/main/kotlin/com/example/testfordatabase/application/service/ProfilesController.kt
@@ -2,6 +2,7 @@ package com.example.testfordatabase.application.service
 
 import com.example.testfordatabase.application.exception.UserNotFoundException
 import com.example.testfordatabase.application.util.BaseService
+import com.example.testfordatabase.domain.aggregate.follow.FollowRelation
 import com.example.testfordatabase.domain.aggregate.follow.FollowRelationId
 import com.example.testfordatabase.domain.aggregate.follow.FollowRelationRepository
 import com.example.testfordatabase.domain.aggregate.user.MyUser
@@ -30,13 +31,12 @@ class ProfilesController(
 ) : BaseService() {
     @GetMapping("/profiles/{username}")
     fun getProfileByUsername(@PathVariable username: String?): ResponseEntity<ProfileResponseData?>? {
-        print("buuuka")
         val currentUser: MyUser? = authenticationService.currentMyUser
-        val func : (MyUser)->Boolean = {user->
+        val func: (MyUser) -> Boolean = { user ->
             currentUser?.let {
                 followRelationRepository
                     .findById(FollowRelationId(it.id, user.id)).isPresent
-            }?:false
+            } ?: false
         }
 
         return userRepository.findByUsername(username)?.let {
@@ -45,21 +45,23 @@ class ProfilesController(
             throw UserNotFoundException(username)
         }
     }
-@PostMapping("/profiles/{username}/follow")
+
+    @PostMapping("/profiles/{username}/follow")
     fun followUserByUsername(@PathVariable username: String?): ResponseEntity<ProfileResponseData?>? {
         val currentUser: MyUser = currentUserOrThrow()
         val user: MyUser = userRepository
             .findByUsername(username)
-            ?:run { throw UserNotFoundException(username) }
+            ?: run { throw UserNotFoundException(username) }
 
-//        val followRelationId = FollowRelationId(currentUser.getId(), user.getId())
-//        followRelationRepository
-//            .findById(followRelationId)
-//            .or {
-//                val followRelation = FollowRelation(currentUser.getId(), user.getId())
-//                followRelationRepository.save(followRelation)
-//                Optional.of(followRelation)
-//            }
+        val followRelationId = FollowRelationId(currentUser.id, user.id)
+        followRelationRepository
+            .findById(followRelationId)
+            .or {
+                val followRelation = FollowRelation(FollowRelationId(currentUser.id, user.id))
+                followRelationRepository.save(followRelation)
+                Optional.of(followRelation)
+            }
+
         return ok(toProfileResponse(user, true))
     }
 
