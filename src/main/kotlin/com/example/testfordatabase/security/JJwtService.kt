e5c60fd2d12cab01e2b505d6ac52256966dfc005
nurseiit.tursunkulov <nurseiit.tursunkulov@check24.de>
2023-06-19 22:29:42 +0200

    removed security

--- src/main/kotlin/com/example/testfordatabase/security/JJwtService.kt
+++ src/main/kotlin/com/example/testfordatabase/security/JJwtService.kt
@@ -1,57 +1,57 @@
-package com.example.testfordatabase.security
-
-import com.example.testfordatabase.domain.aggregate.user.MyUser
-import com.example.testfordatabase.domain.aggregate.user.UserRepository
-import io.jsonwebtoken.Jwts
-import io.jsonwebtoken.SignatureAlgorithm
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
-import org.springframework.security.crypto.password.PasswordEncoder
-import org.springframework.stereotype.Component
-import java.util.*
-
-@Component
-class JJwtService
-/**
- * Creates DefaultJwtService instance.
- *
- * @param secret jwt secret
- * @param sessionTime jwt session time in seconds
- * @param userRepository user repository
- */ @Autowired constructor(
-    @param:Value("\${jwt.secret}") private val secret: String,
-    @param:Value("\${jwt.sessionTime}") private val sessionTime: Int,
-    private val userRepository: UserRepository
-) : JwtService {
-    /** {@inheritDoc}  */
-    override fun generateToken(myUser: MyUser?): String? {
-        return Jwts.builder()
-            .setSubject(myUser?.id?.let { java.lang.Long.toString(it) })
-            .setExpiration(Date(System.currentTimeMillis() + sessionTime * 1000))
-            .signWith(SignatureAlgorithm.HS512, secret)
-            .compact()
-    }
-
-    /** {@inheritDoc}  */
-    override fun getUser(token: String?): Optional<MyUser?>? {
-        return try {
-            val subject = Jwts.parser().setSigningKey(secret).parseClaimsJws(token).body.subject
-            val userId = subject.toLong()
-            userRepository.findById(userId)
-        } catch (e: Exception) {
-           null
-        }
-    }
-}
-
-@Configuration
-class SecurityConfig {
-
-    @Bean
-    fun passwordEncoder(): PasswordEncoder {
-        return BCryptPasswordEncoder()
-    }
-}
+//package com.example.testfordatabase.security
+//
+//import com.example.testfordatabase.domain.aggregate.user.MyUser
+//import com.example.testfordatabase.domain.aggregate.user.UserRepository
+//import io.jsonwebtoken.Jwts
+//import io.jsonwebtoken.SignatureAlgorithm
+//import org.springframework.beans.factory.annotation.Autowired
+//import org.springframework.beans.factory.annotation.Value
+//import org.springframework.context.annotation.Bean
+//import org.springframework.context.annotation.Configuration
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+//import org.springframework.security.crypto.password.PasswordEncoder
+//import org.springframework.stereotype.Component
+//import java.util.*
+//
+//@Component
+//class JJwtService
+///**
+// * Creates DefaultJwtService instance.
+// *
+// * @param secret jwt secret
+// * @param sessionTime jwt session time in seconds
+// * @param userRepository user repository
+// */ @Autowired constructor(
+//    @param:Value("\${jwt.secret}") private val secret: String,
+//    @param:Value("\${jwt.sessionTime}") private val sessionTime: Int,
+//    private val userRepository: UserRepository
+//) : JwtService {
+//    /** {@inheritDoc}  */
+//    override fun generateToken(myUser: MyUser?): String? {
+//        return Jwts.builder()
+//            .setSubject(myUser?.id?.let { java.lang.Long.toString(it) })
+//            .setExpiration(Date(System.currentTimeMillis() + sessionTime * 1000))
+//            .signWith(SignatureAlgorithm.HS512, secret)
+//            .compact()
+//    }
+//
+//    /** {@inheritDoc}  */
+//    override fun getUser(token: String?): Optional<MyUser?>? {
+//        return try {
+//            val subject = Jwts.parser().setSigningKey(secret).parseClaimsJws(token).body.subject
+//            val userId = subject.toLong()
+//            userRepository.findById(userId)
+//        } catch (e: Exception) {
+//           null
+//        }
+//    }
+//}
+//
+//@Configuration
+//class SecurityConfig {
+//
+//    @Bean
+//    fun passwordEncoder(): PasswordEncoder {
+//        return BCryptPasswordEncoder()
+//    }
+//}
