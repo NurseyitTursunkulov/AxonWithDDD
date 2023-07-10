e202fa55224a8616a7f5cd927f07b2121d8f66c7
nurseiit.tursunkulov <nurseiit.tursunkulov@check24.de>
2023-07-10 14:35:48 +0200

    added logging

--- src/main/kotlin/com/example/testfordatabase/TestForDatabaseApplication.kt
+++ src/main/kotlin/com/example/testfordatabase/TestForDatabaseApplication.kt
@@ -1,12 +1,23 @@
 package com.example.testfordatabase
 
+import org.slf4j.LoggerFactory
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.security.access.SecurityConfig
 
 @SpringBootApplication
-class TestForDatabaseApplication
+class TestForDatabaseApplication{
+	@Bean
+	fun init(): CommandLineRunner? {
+		return CommandLineRunner { evt: Array<String?>? ->
+			logger.debug("hallo")
+			logger.debug("noch mal hallo")
+		}
+	}
+}
+val logger = LoggerFactory.getLogger(TestForDatabaseApplication::class.java)
 
 fun main(args: Array<String>) {
 	runApplication<TestForDatabaseApplication>(*args)
